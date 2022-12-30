99c2bad803924eb3cb11508c90acfc6dca7eebfd
Andrew Jackson <dorkmeisterx69@gmail.com>
2022-12-30 11:07:23 -0600

    checkin kustomize code

--- install-kustomize.sh
+++ install-kustomize.sh
@@ -5,9 +5,6 @@
 # kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 # kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
 
-# kubectl apply -n tekton -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.9/git-clone.yaml
-
-kubectl apply -n tekton-pipelines -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.9/git-clone.yaml
 kustomize build ./helm/home-k8s | kubectl apply --prune --overwrite --all --wait -f -
 
 # kustomize build ./helm/home-k8s | kubectl delete -f -
