e8694c70bf45690bc38f55e1fb415c4a4a84fdb9
Andrew Jackson <dorkmeisterx69@gmail.com>
2023-01-06 20:08:23 -0600

    add adguard to kustomize

--- install-kustomize.sh
+++ install-kustomize.sh
@@ -5,6 +5,6 @@
 # kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 # kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
 
-kustomize build ./helm/home-k8s | kubectl apply --prune --overwrite --all --wait -f -
+kustomize build ./helm/home-k8s --enable-helm | kubectl apply --prune --overwrite --all --wait -f -
 
 # kustomize build ./helm/home-k8s | kubectl delete -f -
